0fed3766e57372ee1614475e2356dd31c157166d
jeffoowz <jeffoowz@gmail.com>
2025-07-07 16:51:46 +0800

    longest substring without repeating characters

--- src/main/java/s3_longest_substring_without_repeating_characters/Solution.java
+++ src/main/java/s3_longest_substring_without_repeating_characters/Solution.java
@@ -1,24 +1,31 @@
 package s3_longest_substring_without_repeating_characters;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int length = s.length();
-        int currentMax = 0;
-        Map<String, String> temp = new HashMap<String, String>();
-        for (i: s.toCharArray()){
-            if (temp.containsKey(i)){
+        HashSet<Character> chars = new HashSet<>();
+        int l = 0, res = 0;
 
-            } else {
-                temp.put(i, i);
+        for (int r = 0; r < s.length(); r++) {
+            while (chars.contains(s.charAt(r))) {
+                chars.remove(s.charAt(l));
+                l++;
             }
+            chars.add(s.charAt(r));
+            res = Math.max(res, r - l + 1);
         }
+
+        return res;
     }
 
     public static void main(String[] args) {
         Solution ts = new Solution();
+        int testCase1 = ts.lengthOfLongestSubstring("dvdf");
+        System.out.println(testCase1);
 
     }
 }
